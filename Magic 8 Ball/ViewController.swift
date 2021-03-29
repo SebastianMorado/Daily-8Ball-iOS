8fa095d22d1528b2d28ea6162622c19f42fe020a
SebastianMorado <sabby.morado@gmail.com>
2021-03-29 23:56:57 +0800

    add functionality and design

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var questionCount = 5
+    let ballArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball5")]
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelView: UILabel!
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        if questionCount > 0{
+            questionCount -= 1
+            imageView.image = ballArray.randomElement()
+            if questionCount == 0 {
+                labelView.text = "You cannot ask any more questions"
+            } else if questionCount == 1 {
+                labelView.text = "You can ask \(questionCount) more question"
+            } else {
+                labelView.text = "You can ask \(questionCount) more questions"
+            }
+        }
+        
+    }
 }
 
